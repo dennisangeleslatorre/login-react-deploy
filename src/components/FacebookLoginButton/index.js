5a4f8a40891a1e38596bb08c8e5c760de9d600c6
Dennis Angeles <denisarmando203@gmail.com>
2023-04-18 23:40:22 -0500

    Change source fb

--- src/components/FacebookLoginButton/index.js
+++ src/components/FacebookLoginButton/index.js
@@ -23,7 +23,7 @@ const FacebookLoginButton = () => {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) {return;}
             js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/es_LA/sdk.js";
+            js.src = "https://connect.facebook.net/es_LA/all.js";
             fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
         }, []);
